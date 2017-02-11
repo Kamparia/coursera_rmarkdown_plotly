0117c59cfd1370c801e0432670bffe7331828077
Somide Olaoye <somideolaoye@gmail.com>
2017-02-11 22:26:51 +0000

    Updates

---
+++ main.Rprofile
@@ -0,0 +1,2 @@
+Sys.setenv("plotly_username"="kamparia")
+Sys.setenv("plotly_api_key"="8o8ZIQzrVGMEqNoytrbA")
---
+++ main.r
@@ -0,0 +1,12 @@
+library(plotly)
+set.seed(100)
+d <- diamonds[sample(nrow(diamonds), 1000), ]
+plot_ly(d, x = ~carat, y = ~price, color = ~carat,
+        size = ~carat, text = ~paste("Clarity: ", clarity))
+
+
+p <- ggplot(data = d, aes(x = carat, y = price)) +
+  geom_point(aes(text = paste("Clarity:", clarity))) +
+  geom_smooth(aes(colour = cut, fill = cut)) + facet_wrap(~ cut)
+
+ggplotly(p)
